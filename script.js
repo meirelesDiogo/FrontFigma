5a0d56c2185064d80fd26f80d0f983daec96eea2
Diogo Alexandre <didialexandre1017@gmail.com>
2025-10-21 08:27:21 -0300

    Scripts do Formulario Adicionados.

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 
-function curriculo(){
-    // Confirmação
-    window.confirm("Deseja baixar meu Currículo em PDF?")
+let num1 = 3;
+for (let num=0;num<=10;num++){
+   
+    console.log(num1*num);
+    
 }
 
+
+
+function InpBotao(){
+    event.preventDefault(); //Evita q a  pagina atualize qnd o form for enviado 
+    let InpNome = document.getElementById("Nome").value;
+    let InpEmail = document.getElementById("Email").value;
+
+    if (InpNome === "" || InpEmail	=== ""){
+        alert("Por favor, preencha os campos Nome e Email.");
+
+    }
+    else{
+        alert("Pronto " + InpNome + " Formulário enviado com sucesso!");
+
+
+        console.log("O nome digitado foi "+ InpNome)
+        console.log("O email digitado foi "+ InpEmail)
+    }
+
+
+}
